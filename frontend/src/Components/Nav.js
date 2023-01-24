3c6197fbdbe1f273fa69033fd4ede074f87ec7be
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2023-01-23 19:18:15 -0600

    home page css

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -5,15 +5,9 @@ const Nav = () => {
         <>
         {/* <img src={logo} alt="logo" />
         <div>About Us</div> */}
-        <nav className="flex sm:justify-center space-x-4">
-            {[
-                ['Home', '/dashboard'],
-                ['Team', '/team'],
-                ['Projects', '/projects'],
-                ['Reports', '/reports'],
-            ].map(([title, url]) => (
-                <a href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
-            ))}
+        <nav className="flex sm:justify-around space-x-4 bg-light-pink">
+            <a href='/' className="rounded-lg px-3 py-2 font-medium hover:bg-green hover:text-yellow text-green">Home</a>
+            <a href='/aboutus' className="rounded-lg px-3 py-2 font-medium hover:bg-green hover:text-yellow text-green">About Us</a>
         </nav>
         </>
     )
