d05be3d45070ab49ecae08e6fed85de42f21225f
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2023-01-25 17:06:33 -0600

    minor changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Modal from 'react-modal';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+Modal.setAppElement(root)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
