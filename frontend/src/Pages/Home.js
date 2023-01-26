76bd05a3842b0e619a7605d15fd34db90e38835c
Andrew <andrew.liu.722@gmail.com>
2023-01-25 19:02:02 -0800

    started on working on making group form dynamic

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,22 +1,38 @@
-
+import { useState } from "react";
 const Home = () => {
-    return (
-      <div className="h-screen flex-col items-center font-worksans bg-yellow">
-        <div className="flex justify-center items-end h-1/2">
-          <h1 className="text-center font-bold text-3xl">I am...</h1>
-        </div>
-        <div className="flex justify-center space-x-4 mt-6">
-            <button
-              className="w-50 bg-white tracking-wide text-green font-bold rounded border-b-2 border-green hover:border-green hover:bg-light-pink hover:text-green shadow-md py-2 px-6 inline-flex items-center">
-              <span className="mx-auto">Starting a Group</span>
-            </button>
-            <button
-              className="w-50 bg-white tracking-wide text-green font-bold rounded border-b-2 border-green hover:border-green hover:bg-light-pink hover:text-green shadow-md py-2 px-6 inline-flex items-center">
-              <span className="mx-auto">Joining a Group</span>
-            </button>
-        </div>
+  const [newGroup, setNewGroup] = useState(false)
+  const [joinGroup, setJoinGroup] = useState(false)
+
+  // need to add link to form while passing in newGroup or joinGroup variables so that form can change based on values passed in
+  function clickNewGroup(){
+    setNewGroup(true)
+    setJoinGroup(false)
+  }
+
+  function clickJoinGroup(){
+    setNewGroup(false)
+    setJoinGroup(true)
+  }
+
+  return (
+    <div className="h-screen flex-col items-center font-worksans bg-yellow">
+      <div className="flex justify-center items-end h-1/2">
+        <h1 className="text-center font-bold text-3xl">I am...</h1>
       </div>
-    );
+      <div className="flex justify-center space-x-4 mt-6">
+          <button 
+            onClick={clickNewGroup}
+            className="w-50 bg-white tracking-wide text-green font-bold rounded border-b-2 border-green hover:border-green hover:bg-light-pink hover:text-green shadow-md py-2 px-6 inline-flex items-center">
+            <span className="mx-auto">Starting a Group</span>
+          </button>
+          <button
+            onClick={clickJoinGroup}
+            className="w-50 bg-white tracking-wide text-green font-bold rounded border-b-2 border-green hover:border-green hover:bg-light-pink hover:text-green shadow-md py-2 px-6 inline-flex items-center">
+            <span className="mx-auto">Joining a Group</span>
+          </button>
+      </div>
+    </div>
+  );
   }
   
   export default Home;
