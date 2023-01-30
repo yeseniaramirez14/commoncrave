d0eeb2d1d6b54f14445201ce08a77eefadd10a44
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2023-01-30 16:52:47 -0600

    worked on locate me feature

--- frontend/src/Components/GroupForm.js
+++ frontend/src/Components/GroupForm.js
@@ -1,33 +1,64 @@
 import { useState } from "react";
 import Modal from "./Modal";
+import { useLocation } from 'react-router-dom';
+
 
 const GroupForm = () => {
+    const {state} = useLocation();
     const [openModal, setOpenModal] = useState(false)
+    const [isLocated, setIsLocated] = useState(false)
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    const [status, setStatus] = useState(null);
 
-    let x = document.getElementById("location")
-    function getLocation() {
-        if (navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(showPosition);
+
+    // install dotenv webpack 
+    // npm install dotenv-webpack --save-dev
+
+
+    const getLocation = async () => {
+        if (!navigator.geolocation) {
+            setStatus('Geolocation is not supported by your browser');
         } else {
-            x.innerHTML = "Unable to find location"
+            setStatus('Locating...');
+            navigator.geolocation.getCurrentPosition((position) => {
+                setStatus(null);
+                setLat(position.coords.latitude);
+                setLng(position.coords.longitude);
+                setIsLocated(true);
+            }
+            , () => {
+                setStatus("Unable to retrieve your location");
+            }) 
+            if (isLocated) {
+                const res = await fetch(
+                    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+                );
+                if (!res.ok) {
+                    throw new Error('Location fetch was not okay')
+                }
+                console.log("RESSSS HEREEEE", res.json())
+                return res.json();
+            }
         }
-    }
-    function showPosition(position) {
-        x.innerHTML = "Latitude " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
     }
 
 
     return (
         <div className="h-screen justify-center font-worksans bg-yellow flex-col items-center">
-            <h1 className="text-center pt-10 font-bold text-3xl">placeholder</h1>
+            <h1 className="text-center pt-10 font-bold text-3xl">
+                {state.isNewGroup ? 'Start Group' : 'Join Group'}
+            </h1>
             <form className="pt-5">
                 <div className="flex justify-center flex-wrap -mx-3 mb-6">
                     <div className="w-half px-3">
-                        <label className="block tracking-wide text-black font-bold mb-2" htmlFor="groupName">
-                            PLACEHOLDER
-                            <input name="groupName" id="groupName" className="appearance-none block w-full bg-white text-black border border-green rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="PLACEHOLDER" />
-                        </label>
+                        {state.isNewGroup ? <label className="block tracking-wide text-black font-bold mb-2" htmlFor="groupName">
+                            Group Name
+                            <input name="groupName" id="groupName" className="appearance-none block w-full bg-white text-black border border-green rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Group Name" />
+                        </label> : <label className="block tracking-wide text-black font-bold mb-2" htmlFor="groupId">
+                            Group ID
+                            <input name="groupId" id="groupId" className="appearance-none block w-full bg-white text-black border border-green rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Group ID" />
+                        </label>}
                     </div>
                 </div>
                 <div className="flex justify-center flex-wrap -mx-3 mb-6">
@@ -38,15 +69,16 @@ const GroupForm = () => {
                         </label>
                     </div>
                 </div>
-                <div className="flex justify-center flex-wrap -mx-3 mb-6">
+                <div className="flex justify-center flex-wrap -mx-3">
                     <div className="w-half px-3">
-                        <label className="block tracking-wide text-black font-bold mb-2" htmlFor="address">
+                        <label className="block tracking-wide text-black font-bold" htmlFor="address">
                             Address
-                            <input name="address" id="address" className="appearance-none block w-full bg-white text-black border border-green rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Address" />
+                            <input name="address" id="address" className="appearance-none block w-full bg-white text-black border border-green rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Address" />
                         </label>
-                    </div>
-                    <div>
-                        <p id="location" onClick={getLocation()}>Locate Me</p>
+                        <button type="button" className="mb-6 hover:text-pink" id="location" onClick={getLocation}>Locate Me</button>
+                        {/* <p>{status}</p>
+                        {lat && <p>Latitude: {lat}</p>}
+                        {lng && <p>Longitude: {lng}</p>} */}
                     </div>
                 </div>
                 <div className="flex justify-center flex-wrap -mx-3 mb-6">
@@ -59,7 +91,9 @@ const GroupForm = () => {
                     </div>
                 </div>
                 <div className="flex justify-center">
-                    <button type="submit" className="shadow bg-pink hover:bg-dark-pink focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">Submit</button>
+                    <button type="submit" className="shadow bg-pink hover:bg-dark-pink focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                        {state.isNewGroup ? 'Create' : 'Join'}
+                    </button>
                 </div>
             </form>
         </div>
