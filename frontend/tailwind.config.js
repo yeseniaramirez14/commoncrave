2853f0cc3ffb3dc696873b61b3619428347566ae
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2023-02-02 19:52:34 -0600

    checkbox updates when a restaurant is entered and text bubble hover over cravings button when address is not entered

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,24 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        "worksans": ["Work Sans", "sans-serif"]
-      }
+        worksans: ["Work Sans", "sans-serif"],
+      },
     },
     colors: {
-      "yellow": "#FFF3B2",
+      yellow: "#FFF3B2",
       "light-pink": "#FFE0D8",
-      "pink": "#FF9B93",
-      "green": "#41584B",
-      "white": "#FFFFFF",
-      "black": "#000000",
+      pink: "#FF9B93",
+      green: "#41584B",
+      white: "#FFFFFF",
+      black: "#000000",
       "dark-pink": "#FF5562",
-      "red": "#ff0000"
-    }
+      red: "#ff0000",
+      grey: "#BEBEBE",
+    },
   },
   plugins: [],
-}
+};
