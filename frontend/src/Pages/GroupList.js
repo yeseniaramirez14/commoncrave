335e70caad0c29ff20e5eee151be1e046ce850df
Andrew <andrew.liu.722@gmail.com>
2023-02-21 12:46:49 -0800

    fixed get call for users of the same group, added a loading state for the group list, added a refresh interval for the group list.

--- frontend/src/Pages/GroupList.js
+++ frontend/src/Pages/GroupList.js
@@ -1,25 +1,53 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react"
 
 const GroupList = () => {
+  const { id } = useParams();
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const getUsers = async () =>{
     const res = await fetch(
-      `${process.env.REACT_APP_API_HOST}/api/user`,
+      `${process.env.REACT_APP_API_HOST}/api/group/${id}/users`,
     )
     if (!res.ok) {
       throw new Error("Could not fetch all users");
     }
     let data = await res.json();
-    console.log("all users:", data)
+    setUsers(data.users)
   }
 
-  getUsers()
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getUsers()
+      setLoading(false)
+    },5000)
+    return () => {
+      // clears interval before running new effect
+      clearInterval(interval)
+    }
+  },[]);
 
-  const { id } = useParams();
+ // for testing we're using group 63e6d45c982202426c98cc09
+
   return(
     <>
+      <div> {loading ? "loading": "done loading"}</div>
       <div>You have successfully created/joined a group {id}</div>;
-      <button>Everyone's Here</button>
+      <div>
+        <h1>Add friends to your group!</h1>
+        Unique url link here
+        {`${process.env.REACT_APP_API_HOST}/joingroup/${id}`}
+      </div>
+      <div>
+        <h1>Friends that have joined</h1>
+        {users.map(user => {
+          return (
+            <p>{users.indexOf(user)+1}. { user.name }</p>
+          )
+        })}
+      </div>
+      <button>Get Results</button>
     </>
     )
 };
---
+++ frontend/src/Pages/JoinGroupFromUrlForm.js
@@ -0,0 +1,8 @@
+const JoinGroupFromUrlForm = () => {
+    return(
+        <>
+        </>
+    )
+}
+
+export default JoinGroupFromUrlForm
