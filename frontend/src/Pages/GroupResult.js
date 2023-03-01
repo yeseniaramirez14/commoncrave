04636576ae51421fd9c637f1f69c43f2d398ee45
Andrew <andrew.liu.722@gmail.com>
2023-03-01 14:02:04 -0800

    starting to add to the group list

--- frontend/src/Pages/GroupResult.js
+++ frontend/src/Pages/GroupResult.js
@@ -1,10 +1,40 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom";
 
 const GroupList = () => {
+    const { id } = useParams();
     const [groupCraving, setGroupCraving] = useState("")
+    const [cravingsDict, setCravingsDict] = useState({})
+    const [users, setUsers] = useState([])
+
+
+    useEffect(() => {
+        getAllUserCravings()
+    },[])
+
+
     // Get every craving from each user, create a dictionary that counts each instance, get max
     // if there is a tie, create an array of tied cravings, use randomint between 0-array.length-1
+    const getAllUserCravings = async () => {
+        const res = await fetch(`${process.env.REACT_APP_API_HOST}/api/group/${id}/users`)
+        if (!res.ok) {
+            throw new Error("Could not fetch all users");
+          }
+        let data = await res.json();
+        setUsers(data.users);
 
+        for (let user of users) {
+            let userCravings = user.cravings
+            for (let craving of userCravings) {
+                if (craving in cravingsDict) {
+                    cravingsDict[craving] ++
+                } else {
+                    cravingsDict[craving] = 1
+                }
+            }
+        }
+        console.log(cravingsDict)
+    }
 
     // get average lat/lon of all users
 
