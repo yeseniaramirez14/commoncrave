b89a9c09a38b519a07538dc96607f59d9388a53c
Andrew <andrew.liu.722@gmail.com>
2023-03-01 16:31:14 -0800

    made progress on the results page, made a function to grab the highest picked craving, or choose randomly if there's a tie. added a useRef to useEffect to make it only render once, need to refactor the code as well as add the google map and pin functionality.

--- frontend/src/Pages/GroupResult.js
+++ frontend/src/Pages/GroupResult.js
@@ -1,28 +1,47 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { useParams } from "react-router-dom";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
 
 const GroupResult = () => {
     const { id } = useParams();
-  const [groupCraving, setGroupCraving] = useState("");
-    const [cravingsDict, setCravingsDict] = useState({})
+    const [groupCraving, setGroupCraving] = useState([]);
     const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true);
+    const [avgLat, setAvgLat] = useState(0)
+    const [avgLon, setAvgLon] = useState(0)
+    const [restaurants, setRestaurants] = useState([])
+    const fetchedRef = useRef(false)
 
 
     useEffect(() => {
-        getAllUserCravings()
+        // make sure useEffect only runs once on page load
+        if (fetchedRef.current) return;
+        fetchedRef.current = true;
+
+        getAllUserInfo()
+        setLoading(false)
     },[])
 
 
   // Get every craving from each user, create a dictionary that counts each instance, get max
   // if there is a tie, create an array of tied cravings, use randomint between 0-array.length-1
-    const getAllUserCravings = async () => {
+    const getAllUserInfo = async () => {
         const res = await fetch(`${process.env.REACT_APP_API_HOST}/api/group/${id}/users`)
         if (!res.ok) {
             throw new Error("Could not fetch all users");
-          }
+        }
         let data = await res.json();
+    
         setUsers(data.users);
-        for (let user of users) {
+
+        let cravingsDict = {}
+        let latSum = 0
+        let lonSum = 0
+        let totalUsers = data.users.length
+        for (let user of data.users) {
+            latSum += user.lat
+            lonSum += user.lon
             let userCravings = user.cravings
             for (let craving of userCravings) {
                 if (craving in cravingsDict) {
@@ -32,15 +51,96 @@ const GroupResult = () => {
                 }
             }
         }
-        console.log(cravingsDict)
+        let tempLat = latSum/totalUsers
+        let tempLon = lonSum/totalUsers
+        setAvgLat(latSum/totalUsers)
+        setAvgLon(lonSum/totalUsers)
+        let finalcraving = finalCategory(cravingsDict)
+
+        getRestaurantFromInfo(tempLat, tempLon, finalcraving)
     }
+
+    const getRestaurantFromInfo = async (lati,long,cat) => {
+        console.log(lati, long, cat)
+        const data = await fetch(
+            `${process.env.REACT_APP_API_HOST}/api/alias_to_restaurant`,
+            {
+                method: "post",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ 
+                    lat: lati, 
+                    lon: long, 
+                    category: cat 
+                }),
+              }
+        )
+        if (!data.ok) {
+            throw new Error("Could not get restuarant from info")
+        }
+        let restaurantInfo = await data.json();
+
+        console.log("rest info ", restaurantInfo.data.businesses)
+        setRestaurants(restaurantInfo.data.businesses)
+    }
+
+    function finalCategory(alias_dictionary) {
+        let maxCount = 0;
+        let maxAliases = [];
+        for (let alias in alias_dictionary) {
+          if (alias_dictionary[alias] > maxCount) {
+            maxCount = alias_dictionary[alias];
+            maxAliases = [alias];
+          } else if (alias_dictionary[alias] === maxCount) {
+            maxAliases.push(alias);
+          }
+        }
+        if (maxAliases.length === 1) {
+          setGroupCraving(maxAliases[0])
+          console.log("finalCategory",maxAliases[0])
+          return maxAliases[0]
+        } else {
+            let output = maxAliases[Math.floor(Math.random() * maxAliases.length)]
+            setGroupCraving(output);
+            console.log("finalCategory",output)
+            return output
+        }
+      };
+
 
   // get average lat/lon of all users
 
   // using groupCraving and average lat/lon, make API call to yelpAPI to get the list of restaurants
 
   // create a function for the try again and put it in a useEffect that sets the restaurant name and location for the google pin
-  return <>Group Result</>;
+  return (
+    <>
+        {loading ? (
+            <div className="flex items-center justify-center h-screen bg-yellow">
+                <PacmanLoader />
+            </div>
+        ) : (
+            <div>
+                {users.map((user) => {
+                    return (
+                        <p key={users.indexOf(user) + 1}>
+                        {users.indexOf(user) + 1}. {user.name} + {user.cravings}
+                        </p>
+                    );
+                    })}
+            <div>
+                The Common Crave is... {groupCraving}
+            </div>
+            <div>
+                We recommend to visit: {restaurants.map((restaurant) => {
+                    return (
+                        <p key={restaurant.id}>{restaurant.name}</p>
+                    )
+                })}
+            </div>
+            </div>
+        )}
+    </>
+    );
 };
 
 export default GroupResult;
