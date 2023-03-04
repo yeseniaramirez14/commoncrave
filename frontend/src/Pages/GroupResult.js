c4e1b1aa9b5c3feb4ecdc55c3ced9c62fdfbe8cc
Andrew <andrew.liu.722@gmail.com>
2023-03-04 15:27:44 -0800

    added CSS Styling to the results page

--- frontend/src/Pages/GroupResult.js
+++ frontend/src/Pages/GroupResult.js
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react"
 import { useParams } from "react-router-dom";
 import PacmanLoader from "react-spinners/PacmanLoader";
 import GoogleMapComponent from "../Components/GoogleMapComponent";
+import cravingsData from "../cravingsData.json"
 
 
 
@@ -64,31 +65,29 @@ const GroupResult = () => {
         </div>
     )
    }
-  // create a function for the try again and put it in a useEffect that sets the restaurant name and location for the google pin
-  return (
-    <>
-        {loading ? (
-            <div className="flex items-center justify-center h-screen bg-yellow">
-                <PacmanLoader />
-            </div>
-        ) : (
-            <>
-            <div>
-                <div>
-                    The Common Crave{finalGroup.final_cravings.length === 1 ? ` is... ${finalGroup.final_cravings[0]}`: "s are " + finalGroup.final_cravings.map(craving => (` ${craving}`))}
+
+   return (
+        <>
+            <div className="flex flex-col items-center h-screen bg-yellow">
+                <div className="mt-20 h-20 flex flex-row items-center text-3xl font-semibold">
+                    The Common Crave{finalGroup.final_cravings.length === 1 ? ` is...`: "s are "}
                 </div>
-                <div>
-                    We recommend to visit: {finalGroup.finalRestaurants[finalGroup.restaurant_idx]["name"]}
+                <div className="h-32 flex flex-row items-center text-4xl font-bold">
+                    {finalGroup.final_cravings.length === 1 ? `${finalGroup.final_cravings[0]}` : finalGroup.final_cravings.map( (craving,index) => {return(<span key={index}>{cravingsData[craving]}{index < finalGroup.final_cravings.length - 1 ? ",\u00A0" : " " }</span>)})}
                 </div>
-                <button onClick={tryAgainOnClick}>Try Again</button>
+                <GoogleMapComponent 
+                    lat={finalGroup.finalRestaurants[finalGroup.restaurant_idx]["coordinates"]["latitude"]}
+                    lon={finalGroup.finalRestaurants[finalGroup.restaurant_idx]["coordinates"]["longitude"]}
+                />
+                <div className="mt-10 h-10 flex flex-row items-center text-3xl font-semibold">
+                    We recommend to visit:
+                </div>
+                <div className="mt-10 mb-10 h-10 flex flex-row items-center text-3xl font-bold">
+                    <a href={`${finalGroup.finalRestaurants[finalGroup.restaurant_idx]["url"]}`}>{finalGroup.finalRestaurants[finalGroup.restaurant_idx]["name"]}</a>
+                </div>
+                <button onClick={tryAgainOnClick} className="w-50 bg-white tracking-wide text-green font-bold rounded border-b-2 border-green hover:border-green hover:bg-light-pink hover:text-green shadow-md py-2 px-6 inline-flex items-center">Try Again</button>
             </div>
-            <GoogleMapComponent 
-                lat={finalGroup.finalRestaurants[finalGroup.restaurant_idx]["coordinates"]["latitude"]}
-                lon={finalGroup.finalRestaurants[finalGroup.restaurant_idx]["coordinates"]["longitude"]}
-            />
-            </>
-        )}
-    </>
+        </>
     );
 };
 
