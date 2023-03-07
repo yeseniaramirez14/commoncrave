93a4219c71a8e6582a9c043f2d4f0dc3e0ffe5af
Andrew <andrew.liu.722@gmail.com>
2023-03-06 16:50:52 -0800

    merge fix

--- app.js
+++ app.js
@@ -40,7 +40,3 @@ mongoose
     console.log(err);
   });
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
